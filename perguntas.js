7971c1038a7cf4bae594c510303d91c1d03bcfdb
Elisandradacuaba <168486950+Elisandradacuaba@users.noreply.github.com>
2024-10-09 13:42:40 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,23 @@
 criaCartao(
-    '22',
-    'pq devemos servi ao bolsonaro?',
-    'pq o poder dele é infinito e não conseguimos lutar contra'
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'bolsodivo',
-    'um exemplo do poder de bolsonaro',
-    'quando ele espirra ocorro o famoso tisuname la no japão'
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
-    'jair bolsonaro',
-    'o significado do nome jair bolsonaro?',
-    'força , bondade,esperança e tesão'
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'amor ao bolsonaro',
-    'historia da vida de bolsonaro',
-    'Jair Messias Bolsonaro GOMM (Glicério,[nota 3] 21 de março de 1955) é um militar reformado e político
-    brasileiro, atualmente filiado ao Partido Liberal (PL). Foi o 38.º presidente do Brasil, de 1.º de janeiro de 2019 a 31 de dezembro de 2022, tendo sido eleito pelo Partido Social Liberal (PSL) durante as eleições presidenciais de 2018. Foi deputado federal pelo Rio de Janeiro entre 1991 e 2018. Nasceu em Glicério, mas passou a adolescência em Eldorado, no interior de São Paulo. Começou sua carreira militar no município fluminense de Resende após formar-se na Academia Militar das Agulhas Negras em 1977. Serviu nos grupos de artilharia de campanha e paraquedismo do Exército Brasileiro e tornou-se conhecido do público em 1986, quando escreveu um artigo para a revista Veja criticando os baixos salários dos militares, texto pelo qual foi preso e detido por quinze dias. Um ano depois, a mesma revista o acusou de planejar plantar bombas em unidades militares, o que ele negou. Após ser condenado em primeira instância, o Superior Tribunal Militar o absolveu dessa acusação em 1988. Transferiu-se para a reserva no mesmo ano com o posto de capitão e concorreu à Câmara Municipal do Rio de Janeiro, sendo eleito vereador como membro do Partido Democrata Cristão (PDC).'
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
