75e9639df96fb2e7f89ff04c113b73fa315717c6
Elisandradacuaba <168486950+Elisandradacuaba@users.noreply.github.com>
2024-10-09 14:18:39 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'bolsodivo',
+    'pq devemos adorar o bolsonaro?',
+    'pq com sua arma de 22cm ele tras paz e prazer para todos nos seu poder é infinito'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'goza em mim bolsonaro',
+    'um exemplo de poder dele',
+    'Apenas com 0% do poder dele pode acabar com a fome da venezuela'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'amor eterno',
+    'devemos temer ao bolsonaro?',
+    'não apenas se vc seja feminista ou lgbt petista caso ao contrario bolsonaro te mostrara todo seu amor'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'fim',
+    'obrigado por ter lido ate aqui agr vire e tera uma surpresa',
+    'chegou o estourador de buceta'
 )
